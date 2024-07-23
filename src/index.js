fee43a473e3ff2fa0c283650105d5280140e7e7f
zzqjawnssla <zzqjawnssla@gmail.com>
2024-07-24 00:08:05 +0900

    pwa적용

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+
+serviceWorkerRegistration.register()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
